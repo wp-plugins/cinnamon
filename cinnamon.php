c518f20641ee2ac564ab17ba3cbc042fd11ff4ea
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-04 09:51:02 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/cinnamon/trunk@675961 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cinnamon.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Cinnamon
+Plugin URI: http://www.artiss.co.uk/cinnamon
+Description: Find in-page text using synonyms
+Version: 1.0
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Add meta to plugin details
+*
+* Add options to plugin meta line
+*
+* @since	1.0
+*
+* @param	string  $links	Current links
+* @param	string  $file	File in use
+* @return   	string		Links, now with settings added
+*/
+
+function cinnamon_set_plugin_meta( $links, $file ) {
+
+	if ( strpos( $file, 'cinnamon.php' ) !== false ) {
+		$links = array_merge( $links, array( '<a href="http://wordpress.org/support/plugin/cinnamon">' . __( 'Support' ) . '</a>' ) );
+		$links = array_merge( $links, array( '<a href="http://www.artiss.co.uk/donate">' . __( 'Donate' ) . '</a>' ) );
+	}
+
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'cinnamon_set_plugin_meta', 10, 2 );
+
+/**
+* Add script to theme
+*
+* Add Cinnamon scripts to the main theme
+*
+* @since	1.0
+*/
+
+function cinnamon_add_script() {
+
+    wp_enqueue_script( 'cinnamon_script', WP_PLUGIN_URL . '/' . str_replace( basename( __FILE__ ), '', plugin_basename( __FILE__ ) ) . 'js/cinnamon.min.js', '', '', true );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'cinnamon_add_script' );
+
+/**
+* Cinnamon shortcode
+*
+* Shortcode function to output cinnamon script
+*
+* @since	1.0
+*
+* @param	string	$paras		Shortcode parameters
+* @param	string	$content	Word to be cinnamoned!
+* @return	string			Output code
+*/
+
+function cinnamon_shortcode( $paras = '', $content = '' ) {
+
+    // Loop through shortcode parameters and, if found, built the cinnamon word list
+
+    $array = 0;
+    $cinnamon_list = '';
+    while ( $array < count( $paras ) ) {
+	if ( $array > 0 ) { $cinnamon_list .= ','; }
+	$cinnamon_list .= $paras[ $array ];
+	$array++;
+    }
+
+    // If a word list exists, output the cinnamon code
+
+    if ( $cinnamon_list != '' ) { $content = '<span data-cinnamon="' . $cinnamon_list . '">' . $content. '</span>'; }
+
+    return $content;   
+
+}
+
+add_shortcode( 'cinnamon', 'cinnamon_shortcode' );
+?>
